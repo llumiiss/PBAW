4f099115fd2c23b672beda44177c637c6e72e61e
Maksym Litosh <112276286+llumiiss@users.noreply.github.com>
2025-03-10 14:43:54 +0100

    Add files via upload

---
+++ Labratory01/LM_Calc_01/application/calculator.php
@@ -0,0 +1,58 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+$quota = $_REQUEST ['Quota'];
+$time = $_REQUEST ['Time'];
+$stake = $_REQUEST ['Stake'];
+
+if ( ! (isset($quota) && isset($stake) && isset($time))) {
+    $messages [] = 'Missing request parameters.';
+}
+
+
+if ( $quota == "") {
+    $messages [] = 'Didnt get a number1';
+}
+if ( $time == "") {
+    $messages [] = 'Didnt get a number2';
+}
+if ( $stake == "") {
+    $messages [] = 'Didnt get a number3';
+}
+
+
+if (empty( $messages )) {
+
+
+    if (! is_numeric( $quota )) {
+        $messages [] = 'First value is not a number';
+    }
+
+    if (! is_numeric( $time )) {
+        $messages [] = 'Second value is not a number';
+    }
+    if (! is_numeric( $stake )) {
+        $messages [] = 'Third value is not a number';
+    }
+
+}
+
+
+if (empty ( $messages )) {
+
+    $quota = intval($quota);
+    $time = intval($time);
+    $stake = intval($stake);
+
+    if($quota<1000||$quota>5000000){
+        $messages []= 'Incorrect quota';
+    }
+    if($time<1||$time>420){
+        $messages []= 'Incorrect time';
+    }
+    if($stake<0||$stake>700){
+        $messages []= 'Incorrect stake';
+    }
+
+    $month_quote = ($quota+($stake/100*$quota))/$time;
+}
+include 'calculator_view.php';
